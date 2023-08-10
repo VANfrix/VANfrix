873a3ac12b30a2d9f5ffed7c184e826ac0b31da6
VANfrix <ultrafrost2012@yandex.ru>
2023-08-10 11:50:02 +0300

    Второй коммит по Джанго

--- myapp1/views.py
+++ myapp1/views.py
@@ -3,8 +3,8 @@ from myapp1.models import Worker
 
 # Create your views here.
 def index_page(request):
-    # new_worker = Worker(name = "Ананас", second_name = "Мангов", salary = 170000)
-    # new_worker.save()
+    new_worker = Worker(name = "Владимир", second_name = "Петров", salary = 30000)
+    new_worker.save()
     # worker_to_change = Worker.objects.get (id = 6).delete()
     # worker_to_change.second_name = 'Задерищенский'
     # worker_to_change.name = 'Василий'
@@ -14,8 +14,8 @@ def index_page(request):
     all_workers = Worker.objects.all()
     print(all_workers)
     
-    salary_warkers = Worker.objects.filter(salary=50000)
-    print(salary_warkers)
+    # salary_warkers = Worker.objects.filter(salary=50000)
+    # print(salary_warkers)
     
     for i in all_workers:
         print (f"Фамилия: {i.second_name}, Имя: {i.name}, Зарплата: {i.salary}', ID: {i.id}")
